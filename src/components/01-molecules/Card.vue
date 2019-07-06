185350bfea13bdf306da7576488c266d9d3e63e8
tealicious <carterdanielr@gmail.com>
2019-07-06 15:18:26 -0400

    .

--- src/components/01-molecules/Card.vue
+++ src/components/01-molecules/Card.vue
@@ -2,14 +2,19 @@
   <pre-fetch-router-link :page="knife" column="xs-12 s-12 m-6 l-4" class="card">
     <div>
       <div class="image-wrap">
-        <app-image :image="knife.image"/>
+        <!-- <app-image :image="knife.image"/> -->
+        <div
+          class="faux-image"
+          :style="`background-image: url(${setImage(knife.image)})`"
+          :aria-label="setAlt(knife.image)"
+        />
       </div>
       <div class="card-content">
         <h5 v-if="knife.price && knife.for_sale === 'true'">${{knife.price}}</h5>
         <h5 v-else class="_strike-thru">SOLD</h5>
-        <h3>{{knife.uid.replace(/-/g, ' ')}}</h3>
+        <h3 v-line-clamp:43="2">{{knife.uid.replace(/-/g, ' ')}}</h3>
         <div class="excerpt">
-          <p v-line-clamp:36="3">{{knife.description[0].text}}</p>
+          <p v-line-clamp:28="3">{{knife.description[0].text}}</p>
         </div>
         <button class="button small">view knife</button>
       </div>
@@ -18,6 +23,18 @@
 </template>
 <script>
 export default {
-  props: ["knife"]
+  props: ["knife"],
+  methods: {
+    setImage(image) {
+      return window.innerWidth < 768 && image.mobile
+        ? image.mobile.url
+        : image.url;
+    },
+    setAlt(image) {
+      return window.innerWidth < 768 && image.mobile
+        ? image.mobile.alt
+        : image.alt;
+    }
+  }
 };
 </script>
